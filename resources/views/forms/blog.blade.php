96326caad1ad5af01e2fa83ad1b39da82d8774b2
Joseph Keswet <jhezekiah19@gmail.com>
2022-10-27 12:20:15 +0000

    feat:Landing page implementation

--- resources/views/forms/blog.blade.php
+++ resources/views/forms/blog.blade.php
@@ -4,9 +4,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Document</title>
 </head>
 <body>
-    <p>Blog......</p>
+    <div>
+        <div class="flex-container">
+            <img src="https://cdn.vanguardngr.com/wp-content/uploads/2021/03/1_na3zwwQQ43y-SoBiq6Lq5Q.png" alt="" class="header-image">
+            <div>
+                <h1>Read Our Blog</h1>
+                <p>Reproductive health stories from Pathfinder and beyond.</p>
+            </div>
+        </div>
+        <div class="flex-container">
+            <div class="post-large">
+                <img src="https://www.article19.org/wp-content/uploads/2019/05/iStock-1074842604.jpg" alt="" >
+                <p><span>Dr. Tabinda Sarosh named as a Heroine of Health</span> As daughter of two Pakistani journalists, Dr. Tabinda Sarosh was influenced by...</p>
+            </div>
+            <div class="post-small">
+                <img src="https://www.thelancet.com/cms/attachment/e8e20baf-4c97-4c7b-a3c1-3b70eb9293bf/fx1_lrg.jpg" alt="" >
+                <p><span>Dr. Tabinda Sarosh named as a Heroine of Health</span> As daughter of two Pakistani journalists, Dr. Tabinda Sarosh was influenced by...</p>
+            </div>     
+        </div>
+        <div class="flex-container">
+            <div class="post-small">
+                <img src="https://www.thelancet.com/cms/attachment/e8e20baf-4c97-4c7b-a3c1-3b70eb9293bf/fx1_lrg.jpg" alt="" >
+                <p><span>Dr. Tabinda Sarosh named as a Heroine of Health</span> As daughter of two Pakistani journalists, Dr. Tabinda Sarosh was influenced by...</p>
+            </div>
+            <div class="post-large">
+                <img src="https://www.article19.org/wp-content/uploads/2019/05/iStock-1074842604.jpg" alt="" >
+                <p><span>Dr. Tabinda Sarosh named as a Heroine of Health</span> As daughter of two Pakistani journalists, Dr. Tabinda Sarosh was influenced by...</p>
+            </div>
+        </div>
+        <div class="flex-container">
+            <div>
+                <button class="load-more">Load More</button>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,58 +5,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
 </head>
 <body>
     <div class="container">
+      <nav class="nav-container">
+      <h1>Logo</h1>
         <ul class="nav-links">
            @auth
            
            <a href="#">Courses</a>
            <a href="/logout">Logout</a>     
            @else
-           <a class="active" href="/">Home</a>
-           <a href="#about">About Us</a>
-           <a href="#">Contact</a>
-           <a href="/blog">Blog</a>
-           <a href="/signUp">Register</a>
-           <a href="/login">Login</a>    
+           <a href="/" style="color: black">Home</a>
+           <a href="#about" style="color: black">About Us</a>
+           <a href="#" style="color: black">Contact</a>
+           <a href="/blog" style="color: black">Blog</a>
+           <a class='login-btn' href="/login">Login</a>    
+           <a  class="reg-btn" href="/signUp">Register</a>
            @endauth
-           
-            
         </ul>
-    </div>
-
-    <div class="about-us">
-        <h3 id="about">ABOUT</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis mollitia, possimus<br> 
-        perferendis obcaecati nam debitis maxime odit blanditiis. Perspiciatis libero assumenda<br> 
-        ratione doloremque, totam blanditiis rerum sed facilis similique. Debitis maiores, alias <br>
-        saepe neque animi nulla deleniti odit sapiente. Corrupti, error reprehenderit vel aliquid<br> 
-        esse distinctio! Officiis veniam dolores expedita.
-        </p>
-    </div>    
-
-    <!-- Footer section -->
-  <div class="main-footer">
-    <div class="logo-2">
-      <img src="" alt="logo">
-    </div>
-    <div class="nav-links">
-      <ul>
-        <li><a href="#about">About</a></li>
-        <li><a href="#contact">Contact</a></li>
-        <li><a href="#">Article</a></li>
-        <li><a href="/blog">Blog</a></li>
-      </ul>
-    </div>
-    <div class="socials">
-      <p>Reach us on our social media</p>
-      <div class="icon">
-        <a href="#"><img src="" alt="facebook"></a>
-        <a href="#"><img src="" alt="instagram"></a>
-        <a href="#"><img src="" alt="whatsapp"></a>
+      </nav>
+      <div class="hero-section">
+          <div class="donate-container">
+              <h1>Select Donation Amount</h1>
+              <div class="donate-btns">
+                <button class="donate-amount">₦50,000</button>
+                <button class="donate-amount">₦50,000</button>
+                <button class="donate-amount">₦50,000</button>
+                <button class="donate-amount">₦50,000</button>
+              </div>
+              <div class="enroll-container">
+                  <h1>will support enrollment of both genders</h1>
+              </div>
+              <p>We have volunteers that are ready to work day and night to see the sustainable of the project</p>
+              <button class="donate-button">DONATE NOW</button>
+          </div>
+      </div>
+      <div class="middle-section">
+          <h1 class="header">Technology is now and the Future!!</h1>
+          <div class="stat-container">
+              <div class="stat-card">
+                <h1>14% reduction in child marriage</h1>
+              </div>
+          </div>
       </div>
     </div>
+
   </div>
 </body>
 </html>
